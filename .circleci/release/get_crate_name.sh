4af4f0319f29f9250534211b9d9c503f7a0e2fe7
Taiga Nakayama <dora@dora-gt.jp>
2019-10-26 00:38:59 +0900

    test

--- .circleci/release/get_crate_name.sh
+++ .circleci/release/get_crate_name.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ $1 =~ ^(.*)-v([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?(\+[0-9A-Za-z-]+)?$ ]]; then
-  echo ${BASH_REMATCH[1]}
+    echo ${BASH_REMATCH[1]}
+elif [[ $1 =~ ^ilp-node-.*$ ]]; then
+    echo "ilp-node"
+elif [[ $1 =~ ^ilp-cli-.*$ ]]; then
+    echo "ilp-cli"
 fi
